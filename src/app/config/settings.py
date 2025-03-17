1fcb76073f39f1cfc7f7e27c62d0e40584255a30
Kamalesh <kamalcbe86@gmail.com>
2025-03-17 19:15:52 +0000

    Update .env path to use the local directory

--- src/app/config/settings.py
+++ src/app/config/settings.py
@@ -6,7 +6,7 @@ from dotenv.main import dotenv_values
 def get_settings():
     secrets = {}
 
-    local_env_path = "../.env"
+    local_env_path = ".env"
     if path.exists(local_env_path):
         secrets.update(dotenv_values(local_env_path))
     return secrets
