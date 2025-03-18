23947d1c6da796f7dea0dfe975eb5d4b74c871dd
Prajakta <prajaktasurvase.nbnscoe.entc@gmail.com>
2025-03-19 01:19:24 +0530

    added base changes(uow & schema) for upcoming branches(6.2, 6.3, 6.4)

--- src/app/services/unit_of_work.py
+++ src/app/services/unit_of_work.py
@@ -1,6 +1,8 @@
 from abc import ABC
-
+from sqlalchemy.orm import Session
 from src.app.config.database import get_db
+from src.app.repositories.user_repository import UserRepository
+from fastapi import Depends
 
 
 class BaseUnitOfWork(ABC):
@@ -48,3 +50,17 @@ class BaseUnitOfWork(ABC):
         Roll back the current transaction, reverting uncommitted changes.
         """
         self.session.rollback()
+
+class UserUnitOfWork(BaseUnitOfWork):
+    """Unit of Work class for managing user-related transactions."""
+
+    def __init__(self, db):
+        self.session = db  
+        self.user_repository = UserRepository(self.session) 
+
+    def __enter__(self):
+        """Enter the runtime context, returning itself."""
+        return self
+
+def get_user_uow(db: Session = Depends(get_db)) -> UserUnitOfWork:
+    return UserUnitOfWork(db)
