b4bdffa9cb332c98b9bb13f5c181583ca61e2ed3
chaitanya <saichaitanya_v@srmap.edu.in>
2025-03-21 19:23:40 +0530

    Added 3.2-settleup API

--- src/app/routes/v1/settlements_routers.py
+++ src/app/routes/v1/settlements_routers.py
@@ -1,8 +1,19 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
 
-router = APIRouter(tags=["Settlements Routes"])
+from src.app.schemas.settleup_schemas import SettleUpRequest, SettleUpResponse
+from src.app.services.settleup_services import settle_up_service
+from src.app.services.unit_of_work import SettleUpUnitOfWork
+
+router = APIRouter()
 
 
-@router.get("/settle-up/test")
-def testing():
-    return {"message": "Hello World from settle-up!"}
+@router.patch("/settle-up", response_model=SettleUpResponse)
+def settle_up(
+    request: SettleUpRequest,
+):
+    """
+    API endpoint to settle up a transaction.
+    """
+    unit_of_work = SettleUpUnitOfWork()
+    response = settle_up_service(unit_of_work, request.dict())
+    return response
